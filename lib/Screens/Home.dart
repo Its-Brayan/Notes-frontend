1a4cb4c72f03c59fe32941c7b77d2ca0acdc6057
Brayan <brayanmwangi732@gmail.com>
2025-08-18 12:45:12 +0300

    changes on the UI

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -48,22 +48,53 @@ class Home extends StatelessWidget {
               color: Colors.grey.shade100,
             ),
             SizedBox(height: 30),
-           Card(
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: [
+               Expanded(
+                 child: Card(
 
-             color: Colors.grey.shade500,
-             elevation: 20,
-             child: ListTile(
-               tileColor: Colors.grey.shade600,
-               title: Text("Beautiful notes app Ui concept",
-               style: TextStyle(
-                 color: Colors.white,
-               ),),
-               subtitle: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vehicula maximus nisl vitae volutpat. Praesent elit leo, ornare iaculis mattis sed, tempor eu sem. Duis sodales lectus quis venenatis egestas.'),
-               onTap: (){
-                 Navigator.pushNamed(context, '/takenotes');
-               }
-             ),
-             
+                   color: Colors.grey.shade500,
+                   elevation: 20,
+                   child: ListTile(
+
+                     tileColor: Colors.grey.shade600,
+                     title: Text("Beautiful notes app Ui concept",
+                     style: TextStyle(
+                       color: Colors.white,
+                     ),),
+                     subtitle: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vehicula maximus nisl vitae volutpat. Praesent elit leo, ornare iaculis mattis sed, tempor eu sem. Duis sodales lectus quis venenatis egestas.'),
+                     onTap: (){
+                       Navigator.pushNamed(context, '/takenotes');
+                     }
+                   ),
+
+                 ),
+               ),
+               Expanded(
+                 child: Card(
+                   surfaceTintColor: Colors.blue,
+
+                   color: Colors.grey.shade500,
+                   elevation: 20,
+                   child: ListTile(
+                     shape: RoundedRectangleBorder(
+                       borderRadius: BorderRadius.circular(20),
+                     ),
+                       tileColor: Colors.grey.shade600,
+                       title: Text("Beautiful notes app Ui concept",
+                         style: TextStyle(
+                           color: Colors.white,
+                         ),),
+                       subtitle: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vehicula maximus nisl vitae volutpat. Praesent elit leo, ornare iaculis mattis sed, tempor eu sem. Duis sodales lectus quis venenatis egestas.'),
+                       onTap: (){
+                         Navigator.pushNamed(context, '/takenotes');
+                       }
+                   ),
+
+                 ),
+               ),
+             ],
            )
 
           ],
